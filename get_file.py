9c786b1525b891cfff62b2aa9a881af3be2930c9
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2022-12-20 00:50:39 +0300

    Delete get_file.py

--- get_file.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def getdata(passportlist):
-  response = requests.get('https://disk.yandex.ru/d/kVdTaY8TuGvBkw')
-  if response.status_code == 200:
-    print('Success!')
-  elif response.status_code == 404:
-    print('Not Found.')
-  print(response.content)
