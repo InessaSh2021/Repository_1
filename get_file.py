5d05f64d375ea139518cdb767a665822a20e9dbb
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2022-12-19 23:39:54 +0300

    Add files via upload

---
+++ get_file.py
@@ -0,0 +1,15 @@
+import requests
+
+def getdata(passportlist):
+  response = requests.get('https://disk.yandex.ru/d/kVdTaY8TuGvBkw')
+  if response.status_code == 200:
+    print('Success!')
+  elif response.status_code == 404:
+    print('Not Found.')
+  print(response.json())
+  
+  
+  
+
+
+
